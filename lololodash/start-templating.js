e37f85846d3f061c4880f2d92088d7826dc5ac71
Alexis Ortega <alexis.oc3@gmail.com>
2016-07-16 12:24:34 -0600

    Finished ninth exercise

---
+++ lololodash/start-templating.js
@@ -0,0 +1,8 @@
+var _ = require('lodash');
+
+var worker = function(input) {
+    var template = 'Hello <%= name %> (logins: <%= login.length %>)';
+    return  _.template(template)(input);
+};
+
+module.exports = worker;
